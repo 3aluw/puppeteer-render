8900de24a5ca4a921d6bbe2f0e1f71bec87a37a1
Abdou <2000abdallah2014@gmail.com>
2025-08-30 14:31:14 +0100

    skip link generation in case of a cart link

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -2,8 +2,8 @@ import { getLink } from "./get-link.js";
 import { scrapeCart } from "./scrape-cart.js";
 
 export const getProductsInfo = async(url)=>{
-    const cartUrl = await getLink(url);
+    const isCartLink = url.includes("m.shein")
+    const cartUrl = isCartLink ? url : await getLink(url);
     const productObjects = await scrapeCart(cartUrl);
     return productObjects;
 }
-
